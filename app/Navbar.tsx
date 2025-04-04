7ec2023ee34990cac7af19bf8ae23661d1955180
rajaabdulbasit005 <rajaabdulbasit005@gmail.com>
2025-04-04 23:22:39 +0500

    💥 NEW:  Setup Radix UI

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -11,17 +11,17 @@ const Navbar = () => {
     { label: "Issues", href: "/issues" },
   ];
   return (
-    <nav className="flex space-x-6 px-5 border-b h-16 items-center">
+    <nav className="flex space-x-6 px-5 border-b h-16 items-center font-medium mb-5">
       <Link href={"/"}>
-        <BsBugFill className="text-xl hover:text-zinc-300" />
+        <BsBugFill className="text-xl hover:text-zinc-500" />
       </Link>
       <ul className="flex space-x-6">
         {links.map((link) => (
           <Link
             className={classNames({
-              "text-zinc-200 ": link.href !== currentpath,
-              "text-zinc-500 ": link.href === currentpath,
-              "hover:text-zinc-500 transition-colors text-lg": true,
+              "text-zinc-500 ": link.href !== currentpath,
+              "text-zinc-900 ": link.href === currentpath,
+              "hover:text-zinc-900 transition-colors text-lg": true,
             })}
             href={link.href}
           >
