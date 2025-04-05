a77553c96347f777e24ad662936846bb19d888f9
rajaabdulbasit005 <rajaabdulbasit005@gmail.com>
2025-04-05 10:14:37 +0500

    💥 NEW:  Implemented Issue Form Submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,41 @@
-'use client'
-import { Button, TextField } from '@radix-ui/themes';
+"use client";
+import { Button, TextField } from "@radix-ui/themes";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+import { Form, useForm, Controller } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
-const NewIssue = () => {
-  return (
-    <>
-    <div className='max-w-xl space-y-3'>
-        <TextField.Root placeholder='Title'></TextField.Root>
-        <SimpleMDE placeholder='Enter description here'/>
-        <Button>Submit new Issue</Button>
-    </div>
-    </>
-  )
+
+interface IssueForm {
+  title: string;
+  description: string;
 }
 
-export default NewIssue
+const NewIssue = () => {
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const router = useRouter();
+  return (
+    <>
+      <form
+        className="max-w-xl space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          console.log(data);
+          await axios.post("/api/issue", data);
+          router.push("/issues");
+        })}
+      >
+        <TextField.Root placeholder="Title" {...register("title")} />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Enter description here" {...field} />
+          )}
+        />
+        <Button type="submit">Submit new Issue</Button>
+      </form>
+    </>
+  );
+};
+
+export default NewIssue;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,13 @@
-import { Button } from '@radix-ui/themes'
-import Link from 'next/link'
-import React from 'react'
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
+import React from "react";
 
 const IssuesPage = () => {
   return (
-    <Button><Link href='issues/new'>New issue</Link></Button>
-  )
-}
+    <Button>
+      <Link href="issues/new">New issue</Link>
+    </Button>
+  );
+};
 
-export default IssuesPage
+export default IssuesPage;
