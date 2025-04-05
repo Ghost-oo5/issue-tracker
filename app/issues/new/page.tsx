b6e4e1dc51038a371561bd8feda255bda3988cb7
rajaabdulbasit005 <rajaabdulbasit005@gmail.com>
2025-04-05 12:22:28 +0500

    💥 NEW:  Added loading spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,7 +2,7 @@
 import ErrorMessage from "@/app/components/ErrorMessage";
 import { createIssueSchema } from "@/app/validationSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, Spinner, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
@@ -24,6 +24,8 @@ const NewIssue = () => {
   });
   const router = useRouter();
   const [error, setError] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
   return (
     <div className="max-w-xl ">
       <div className="mb-5">
@@ -42,28 +44,38 @@ const NewIssue = () => {
           console.log(data);
           await axios
             .post("/api/issue", data)
-            .then(() => router.push("/issues"))
-            .catch((err) => setError(err.message));
+            .then(() => {
+              router.push("/issues");
+              setIsSubmitting(true);
+            })
+            .catch((err) => {
+              setError(err.message);
+              setIsSubmitting(false);
+            });
         })}
       >
         <TextField.Root placeholder="Title" {...register("title")} />
-        {errors.title && (
-          <ErrorMessage>{errors.title.message}</ErrorMessage>
-        )}
+        {errors.title && <ErrorMessage>{errors.title.message}</ErrorMessage>}
         <Controller
           name="description"
           control={control}
           render={({ field }) => (
-            <SimpleMDE className="mb-0" placeholder="Enter description here" {...field} />
+            <SimpleMDE
+              className="mb-0"
+              placeholder="Enter description here"
+              {...field}
+            />
           )}
         />
         <div>
           {errors.description && (
-          <ErrorMessage>{errors.description.message}</ErrorMessage>
-        )}
+            <ErrorMessage>{errors.description.message}</ErrorMessage>
+          )}
         </div>
 
-        <Button type="submit">Submit new Issue</Button>
+        <Button disabled={isSubmitting} type="submit">
+          Submit new Issue {isSubmitting && <Spinner />}
+        </Button>
       </form>
     </div>
   );
