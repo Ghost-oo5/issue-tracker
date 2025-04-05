6e72430950ea2343740b04b033e5438a133ad64f
rajaabdulbasit005 <rajaabdulbasit005@gmail.com>
2025-04-05 12:01:44 +0500

    👌 IMPROVE: Implemented client-side  validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
@@ -7,43 +7,61 @@ import { useState } from "react";
 import { Form, useForm, Controller } from "react-hook-form";
 import { BiError } from "react-icons/bi";
 import SimpleMDE from "react-simplemde-editor";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchema";
+import { z } from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
-
+type IssueForm = z.infer<typeof createIssueSchema>;
 const NewIssue = () => {
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const router = useRouter();
   const [error, setError] = useState("");
   return (
     <div className="max-w-xl ">
       <div className="mb-5">
-       { error && <Callout.Root color="red">
-          <Callout.Icon>
-            <BiError />
-          </Callout.Icon>
-          <Callout.Text>{error}</Callout.Text>
-        </Callout.Root>}
+        {error && (
+          <Callout.Root color="red">
+            <Callout.Icon>
+              <BiError />
+            </Callout.Icon>
+            <Callout.Text>{error}</Callout.Text>
+          </Callout.Root>
+        )}
       </div>
       <form
         className="space-y-3"
         onSubmit={handleSubmit(async (data) => {
           console.log(data);
-       
-            await axios.post("/api/issue", data).then(()=>router.push("/issues")).catch((err)=>setError(err.message));
-
+          await axios
+            .post("/api/issue", data)
+            .then(() => router.push("/issues"))
+            .catch((err) => setError(err.message));
         })}
       >
         <TextField.Root placeholder="Title" {...register("title")} />
+        {errors.title && (
+          <Text color="red" as="div">{errors.title.message}</Text>
+        )}
         <Controller
           name="description"
           control={control}
           render={({ field }) => (
-            <SimpleMDE placeholder="Enter description here" {...field} />
+            <SimpleMDE className="mb-0" placeholder="Enter description here" {...field} />
           )}
         />
+        <div>
+          {errors.description && (
+            <Text color="red"as="div">{errors.description.message}</Text>
+          )}
+        </div>
+
         <Button type="submit">Submit new Issue</Button>
       </form>
     </div>
