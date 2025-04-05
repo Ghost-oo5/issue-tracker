f9c05a5f5efd963698434cf89bee370ed2639acf
rajaabdulbasit005 <rajaabdulbasit005@gmail.com>
2025-04-06 00:10:05 +0500

    💥 NEW + 🔨 Configure Code: Configured tailwind and added markdown preview

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,6 +3,7 @@ import { prisma } from "@/prisma/client";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import React from "react";
+import Markdown from "react-markdown";
 
 interface Props {
   params: { id: string };
@@ -21,12 +22,14 @@ const IssueDetailsPage = async ({ params: { id } }: Props) => {
   return (
     <>
       <div>
-          <Heading>{issue?.title}</Heading>
-          <Flex gap={'2'} my={'2'}>
-              <IssueStatusBadge status={issue.status}/>
-              <Text>{issue.createdAt.toDateString()}</Text>
-          </Flex>
-          <Card>{issue.description}</Card>
+        <Heading>{issue?.title}</Heading>
+        <Flex gap={"2"} my={"2"}>
+          <IssueStatusBadge status={issue.status} />
+          <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className="prose" mt={'4'}>
+          <Markdown>{issue.description}</Markdown>
+        </Card>
       </div>
     </>
   );
