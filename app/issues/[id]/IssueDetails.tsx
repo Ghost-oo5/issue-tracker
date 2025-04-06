244de0bbe95cf48e0ce25ef3f54b3108c1ae503a
rajaabdulbasit005 <rajaabdulbasit005@gmail.com>
2025-04-06 22:54:20 +0500

    🔨 Refactor Code:  Applied  single responsibility principle

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,27 @@
+import { IssueStatusBadge } from "@/app/components";
+import { Issue } from "@prisma/client";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import Markdown from "react-markdown";
+
+interface Props {
+  issue: Issue;
+}
+
+const IssueDetails = ({ issue }: Props) => {
+  return (
+    <>
+      <div className="w-full">
+        <Heading>{issue?.title}</Heading>
+        <Flex gap={"2"} my={"2"}>
+          <IssueStatusBadge status={issue.status} />
+          <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className="prose" mt={"4"}>
+          <Markdown>{issue.description}</Markdown>
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default IssueDetails;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,7 @@
-import { IssueStatusBadge, prisma } from "@/app/components";
-import { Pencil2Icon } from "@radix-ui/react-icons";
-import { Button, Card, Flex, Heading, Text } from "@radix-ui/themes";
-import Link from "next/link";
+import { prisma } from "@/app/components";
 import { notFound } from "next/navigation";
-import Markdown from "react-markdown";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
 interface Props {
   params: { id: string };
 }
@@ -20,20 +18,8 @@ const IssueDetailsPage = async ({ params: { id } }: Props) => {
   return (
     <>
       <div className="max-w-xl max-sm:flex-col flex  justify-between">
-        <div className="w-full">
-          <Heading>{issue?.title}</Heading>
-          <Flex gap={"2"} my={"2"}>
-            <IssueStatusBadge status={issue.status} />
-            <Text>{issue.createdAt.toDateString()}</Text>
-          </Flex>
-          <Card className="prose" mt={"4"}>
-            <Markdown>{issue.description}</Markdown>
-          </Card>
-        </div>
-        <Button>
-          <Pencil2Icon />
-          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
-        </Button>
+       <IssueDetails issue={issue}/>
+       <EditIssueButton IssueID={issue.id}/>
       </div>
     </>
   );
