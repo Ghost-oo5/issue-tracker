8376c9966da15498126af78d7eab9638728379c7
Ghost-oo5 <rajaabdulbasit005@gmail.com>
2025-05-06 12:26:40 +0500

    🔨 Refactor Code:  refactored code

--- app/issues/[id]/AsigneeSelect.tsx
+++ app/issues/[id]/AsigneeSelect.tsx
@@ -1,39 +1,39 @@
 "use client";
+import { Skeleton } from "@/app/components/index";
 import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { Skeleton } from "@/app/components/index";
 import toast, { Toaster } from "react-hot-toast";
 interface Props {
   issue: Issue;
 }
 
-const AsigneeSelect = ({ issue }: Props) => {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
+const useUsers = () =>
+  useQuery<User[]>({
     queryKey: ["users"],
     queryFn: () => axios.get("/api/users").then((res) => res.data),
     staleTime: 60 * 1000, //60sec
     retry: 3,
   });
+
+const AsigneeSelect = ({ issue }: Props) => {
+  const { data: users, error, isLoading } = useUsers();
   if (error) return null;
   if (isLoading) return <Skeleton />;
+
+  const HandleUserAssign = (userID: string) => {
+    const assignedToUserId = userID === "unassigned" ? null : userID;
+    axios.patch("/api/issue/" + issue.id, { assignedToUserId }).catch(() => {
+      toast.error("Changes could not be saved");
+    });
+  };
+
   return (
     <>
       <Select.Root
         defaultValue={issue.assignedToUserId || ""}
-        onValueChange={(userID) => {
-          const assignedToUserId = userID === "unassigned" ? null : userID;
-          axios
-            .patch("/api/issue/" + issue.id, { assignedToUserId })
-            .catch(() => {
-              toast.error("Changes could not be saved");
-            });
-        }}
+        onValueChange={HandleUserAssign}
       >
         <Select.Trigger placeholder="Assign..." />
         <Select.Content>
